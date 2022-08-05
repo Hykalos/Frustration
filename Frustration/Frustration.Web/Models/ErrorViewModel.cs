18881609a37cfffc29f128a2af5730e99b76df4e
Morten Turn Pedersen <morten.turn-pedersen@monstar-lab.com>
2022-08-05 08:31:46 +0200

    Set up CI and CD pipelines plus template project

---
+++ Frustration/Frustration.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Frustration.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
