ba565e77a8ecd5e45fe12dff8852df96c57bd5f4
Morten Turn Pedersen <morten.turn.pedersen@gmail.com>
2022-08-09 20:05:07 +0200

    Implemented checks for completing round

--- Frustration/Frustration.BusinessLogic/GameService.cs
+++ Frustration/Frustration.BusinessLogic/GameService.cs
@@ -4,6 +4,23 @@ public class GameService : IGameService
 {
     public Game CompleteRound(Game currentGame, IEnumerable<Guid> playerCompletedRoundIds, IEnumerable<(Guid playerId, uint points)>? points)
     {
+        if (!playerCompletedRoundIds.Any())
+            throw new ArgumentException("No players completed the round. At least one player has to complete the round.", nameof(playerCompletedRoundIds));
+
+        if (currentGame.TrackPoints)
+        {
+            if (points == null)
+                throw new ArgumentException("Points should be included", nameof(points));
+
+            if(points.Any(p => p.points % 5 != 0))
+                throw new ArgumentException("Points were not correct. Points should always be a multiple of 5", nameof(points));
+
+            if (points.Count() != currentGame.Players.Count())
+                throw new ArgumentException("Points are missing for 1 or more players", nameof(points));
+        }
+
+
+
         throw new NotImplementedException();
     }
 
